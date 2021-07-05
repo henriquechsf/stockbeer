b2a5f47f536b3ea1662fe4c8a431cbc2bd44c8af
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-07-04 21:48:01 -0300

    implemented methods controller

---
+++ src/main/java/me/henrique/stockbeer/controller/BeerController.java
@@ -0,0 +1,50 @@
+package me.henrique.stockbeer.controller;
+
+import lombok.AllArgsConstructor;
+import me.henrique.stockbeer.dto.BeerDTO;
+import me.henrique.stockbeer.dto.QuantityDTO;
+import me.henrique.stockbeer.exceptions.BeerAlreadyRegisteredException;
+import me.henrique.stockbeer.exceptions.BeerNotFoundException;
+import me.henrique.stockbeer.exceptions.BeerStockExcededException;
+import me.henrique.stockbeer.service.BeerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController {
+
+    private final BeerService beerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExcededException {
+        return beerService.increment(id, quantityDTO.getQuantity());
+    }
+}
